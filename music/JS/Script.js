43c23ce7257f5330e4af1702346d2e02e388a295
sujeet kumar <sujeetbhardwaj9580@gmail.com>
2026-06-26 22:50:30 +0530

    Add Range with Play And Add new Feature in music

--- music/JS/Script.js
+++ music/JS/Script.js
@@ -1,22 +1,50 @@
+
 const arr = [
   { "Title": "Gehra Hua", "img": "gehra-Hua.jpg", "song": "Gehra Hua.mp3" },
   { "Title": "2 States", "img": "2-states.jpg", "song": "MastMagan.mp3" },
-  { "Title": "Kaise Hua", "img": "KaiseHua.jpg", "song": "Kaise Hua.mp3" }
+  { "Title": "Kaise Hua", "img": "KaiseHua.jpg", "song": "Kaise Hua.mp3" },
+  { "Title": "Ishq Jalakar", "img": "Dhurandhar.jpg", "song": "Ishq Jalakar.mp3" }
 ];
 
 let audio = document.querySelector("#audio");
 let img = document.querySelector(".img-box img");
 let title = document.querySelector("#songTitle"); 
 let btn=document.querySelector("#playpausebtn");
+let start=document.querySelector("#start");
+let end=document.querySelector("#end");
+
 let i = 0;
 let range=document.querySelector("#range");
+console.log(Math.floor(Math.random()*4))
+
+audio.ontimeupdate = () => {
+  range.value = Math.floor(audio.currentTime);
+  start.textContent=formatTime(range.value);
+if (audio.currentTime >= audio.duration) {
+    forward();
+  }
+};
+range.oninput = () => {
+  audio.currentTime =range.value;
+};
+
+function formatTime(value){
+let min = Math.floor(value / 60);
+let sec = Math.floor(value % 60);
+return `${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`;
+
+}
+audio.onloadedmetadata = () => {
+  range.max = Math.floor(audio.duration);
+  end.textContent = formatTime(audio.duration);
+};
 
 function playPause() {
   if (btn.classList.contains("fa-play")) {
-    btn.classList.replace("fa-play", "fa-pause");
-    audio.play();
- console.log(audio.currentTime)
-console.log(audio.duration)
+  btn.classList.replace("fa-play", "fa-pause");
+  audio.play();
+  console.log(audio.currentTime)
+  console.log(audio.duration)
   } else {
     btn.classList.replace("fa-pause", "fa-play");
     audio.pause();
@@ -30,7 +58,7 @@ function forward() {
   }
   img.src = "images/" + arr[i].img;
   audio.src = "Song/" + arr[i].song;
-  title.textContent = arr[i].Title; // update title
+  title.textContent = arr[i].Title; 
  if(btn.classList.contains("fa-pause")){
   audio.play();
   }
@@ -47,5 +75,11 @@ function Backward() {
   if(btn.classList.contains("fa-pause")){
   audio.play();
   }
-
 }
+
+let featureBtn = document.querySelector(".fetaure-btn");
+featureBtn.addEventListener("click", function(evt) {
+  if (evt.target.tagName === "I") {
+    evt.target.classList.toggle("active");
+  }
+});
